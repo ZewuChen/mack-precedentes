a721ffa430722af67562944ab44c84b12dfc2808
Gustavo Rorato Gentil <gustavorgentil@outlook.com>
2018-08-13 16:21:11 -0300

    Update: proposal and patreon content

--- resources/views/pages/proposal.blade.php
+++ resources/views/pages/proposal.blade.php
@@ -6,22 +6,35 @@
 
     <div class="py-4">
         <p >
-        	Em 2015, a aprovação do Código de Processo Civil aprofundou um movimento que se iniciou no Brasil ainda na década de 1990: a possibilidade de os Tribunais editarem decisões das quais os juízes inferiores não podem discordar. 
+            Em 2015, a aprovação do Código de Processo Civil aprofundou um movimento que se iniciou no Brasil ainda na década de 1990: a possibilidade de os Tribunais editarem decisões das quais os juízes inferiores não podem discordar. 
         </p>
-		<p >
-			Essas decisões recebem vários nomes: precedentes vinculantes, precedentes obrigatórios, decisões vinculantes etc. Independentemente do nome, sua natureza é clara. Uma vez que o tribunal tenha estabelecido uma compreensão do Direito, utilizando-se de um determinado procedimento, os juízes inferiores não podem expressar convicções distintas, para outros casos que venham a surgir. Eles estão, portanto, vinculados, a replicar a mesma compreensão do direito, se tiverem que analisar outro caso, idêntico ou análogo.
-		</p> 
-		<p >
-			Esse movimento começou com as ações de controle de constitucionalidade, foi ampliado para as súmulas vinculantes, para a análise de repercussão geral do Supremo Tribunal Federal (STF), para os recursos repetitivos do Superior Tribunal de Justiça (STJ) e, agora, para decisões de todos os tribunais do país, inclusive os tribunais de 2º grau (tribunais de justiça e tribunais regionais federais). Essa estrutura passa a existir também nas justiças especializadas (justiça do trabalho, militar e eleitoral). 
-		</p>
-		<p >
-			A proposta deste site é reunir, em um só lugar, essas decisões e permitir que sua consulta, compreensão e discussão possa se fazer em um ambiente de plena liberdade acadêmica e científica, com a consciência de que, em uma sociedade democrática, todas as instituições estão sujeitas a explicar seus posicionamentos perante o cidadão. 
-		</p>
-		<p >O fato de uma decisão ser obrigatoriamente aplicada não implica que não possa ser discutida. </p>
-		<p >
-			Embora haja diversos sites que oferecem ferramentas de pesquisa jurídica, a proposta do Observatório Mackenzie de Precedentes é única e inédita, por duas razões: primeiro, porque ela não reúne apenas decisões do STF e do STJ, mas também dos Tribunais de Justiça de todos os estados da federação e dos cinco Tribunais Regionais Federais. Isso dá ao Observatório abrangência sobre todo o país. Segundo, porque o Observatório seleciona apenas os precedentes vinculantes. Enquanto em outros sites o pesquisador precisa buscar esses precedentes no meio de milhares de outras decisões, o Observatório já faz essa seleção previamente, disponibilizando apenas os casos mais importantes.
-		</p> 
-		<p >O Observatório Mackenzie de Precedentes conta com o financiamento do Fundo Mackenzie de Pesquisa – MackPesquisa. </p>
+        <p >
+            Essas decisões recebem vários nomes: precedentes vinculantes, precedentes obrigatórios, decisões vinculantes etc. Independentemente do nome, sua natureza é clara. Uma vez que o tribunal tenha estabelecido uma compreensão do Direito, utilizando-se de um determinado procedimento, os juízes inferiores não podem expressar convicções distintas, para outros casos que venham a surgir. Eles estão, portanto, vinculados, a replicar a mesma compreensão do direito, se tiverem que analisar outro caso, idêntico ou análogo.
+        </p> 
+        <p >
+            Esse movimento começou com as ações de controle de constitucionalidade, foi ampliado para as súmulas vinculantes, para a análise de repercussão geral do Supremo Tribunal Federal (STF), para os recursos repetitivos do Superior Tribunal de Justiça (STJ) e, agora, para decisões de todos os tribunais do país, inclusive os tribunais de 2º grau (tribunais de justiça e tribunais regionais federais). Essa estrutura passa a existir também nas justiças especializadas (justiça do trabalho, militar e eleitoral). 
+        </p>
+        <p >
+            A proposta deste site é reunir, em um só lugar, essas decisões e permitir que sua consulta, compreensão e discussão possa se fazer em um ambiente de plena liberdade acadêmica e científica, com a consciência de que, em uma sociedade democrática, todas as instituições estão sujeitas a explicar seus posicionamentos perante o cidadão. 
+        </p>
+        <p >O fato de uma decisão ser obrigatoriamente aplicada não implica que não possa ser discutida. </p>
+        <p >
+            Embora haja diversos sites que oferecem ferramentas de pesquisa jurídica, a proposta do Observatório Mackenzie de Precedentes é única e inédita, por duas razões: primeiro, porque ela não reúne apenas decisões do STF e do STJ, mas também dos Tribunais de Justiça de todos os estados da federação e dos cinco Tribunais Regionais Federais. Isso dá ao Observatório abrangência sobre todo o país. Segundo, porque o Observatório seleciona apenas os precedentes vinculantes. Enquanto em outros sites o pesquisador precisa buscar esses precedentes no meio de milhares de outras decisões, o Observatório já faz essa seleção previamente, disponibilizando apenas os casos mais importantes.
+        </p> 
+        <p >O Observatório Mackenzie de Precedentes conta com o financiamento do Fundo Mackenzie de Pesquisa – MackPesquisa. </p>
+
+    </div>
+    <br><br>
+
+    <h2 class="mp-heading">Atuação da Engenharia: Validação e inspeções técnicas do website</h2>
+
+    <div class="py-4" align="justify">
+        <p >
+            Com o crescimento da demanda e complexidade do website observa-se um ponto de atenção para as inspeções e validação de software, que corroboram para a sua qualidade e eficiência. Os dois principais tipos de inspeção a serem considerados são: a revisão do código e a revisão por pares. Na primeira examina-se um trecho de código e corrigem-se todos os eventuais defeitos (considera-se defeito uma parte do código que não é aplicada adequadamente aos requisitos ou que não está de acordo com a proposta, mas pode ser melhorado); ela é muito útil porque ajuda a melhorar as técnicas de programação. Já as avaliações por pares permitem a detecção de defeitos de software até mesmo nos estágios iniciais; ela inclui elementos como: listas de verificação, processo de revisão organizado, definição dos papeis de cada membro, relatórios e formulários. Ambas as técnicas ajudam a manter a ordem no desenvolvimento e podem servir como diferencial, criando um produto enxuto e otimizado.
+        </p>
+        <p >
+            A atuação do aluno bolsista de engenharia será, com base na técnica de avaliação por pares, verificar a ocorrência de defeitos de software, principalmente neste estágio de lançamento do website. Para a inspeção e validação do website o aluno fará uso de: listas de verificação, revisão sistemática das funcionalidades e utilização do site, aferição dos papeis de cada usuário, desenvolvedores e perfis; além da elaboração de relatórios e formulários de teste e verificação.
+        </p> 
 
     </div>
 
